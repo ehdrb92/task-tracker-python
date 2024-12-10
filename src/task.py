369c556e307720b299298f12f6a0d175f016b4d2
Donggyu <98671555+ehdrb92@users.noreply.github.com>
2024-12-10 20:38:59 +0900

    add task entity class status field validate

--- src/task.py
+++ src/task.py
@@ -9,6 +9,21 @@ from task_status import TaskStatus
 class Task:
     id: int
     decription: str
-    status: TaskStatus
     created_at: datetime
     updated_at: datetime
+    status: TaskStatus
+
+    @property
+    def status(self):
+        return self._status
+
+    @status.setter
+    def status(self, value):
+        if value not in ["todo", "in-progress", "done"]:
+            raise ValueError("status must 'todo' or 'in-progress' or 'done'")
+        if value == "todo":
+            self._status = 1
+        if value == "in-progress":
+            self._status = 2
+        if value == "done":
+            self._status = 3
